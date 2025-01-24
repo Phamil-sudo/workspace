44f6935d8a00a79294dbe63f48b94c750d9fb597
Phamil-sudo <tailon.shain@gmail.com>
2025-01-23 20:43:12 -0500

    Add files via upload

---
+++ moviequote/src/moviequote/moviequote.java
@@ -0,0 +1,12 @@
+// Peyton 1/15 --line comment 
+		package moviequote;
+
+public class moviequote {
+
+	public static void main(String[] args) {
+ 		System.out.println("If the boot fits");
+ 		System.out.println("Woody");
+ 		System.out.println("Toy Story");
+ 		System.out.println("1999");
+	}
+}
---
+++ songlyric/src/songslyrics.java
@@ -0,0 +1,11 @@
+// Peyton 1/15 -- line comment
+public class songslyrics {
+
+	public static void main(String[] args) {
+ System.out.println("Been dreaming of getting away since I dont know");
+ System.out.println("Ain't no better time than now, for mexico");
+ System.out.println("No Shoes, No Shirt, No problems");
+	
+	}
+
+}
